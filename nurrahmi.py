b87c6715978ac5d4b85b8edecb27cbd983c63679
Nur Rahmi <49005228+Nurrahmi09@users.noreply.github.com>
2019-10-16 04:30:40 -0700

    Create nurrahmi.py

---
+++ nurrahmi.py
@@ -0,0 +1,7 @@
+import math
+  def PersamaanKuadrat(a, b, c):
+    return (b**2) - (4*a*c)
+  def BarisanAritmatika(a, b, n):
+    return a + (n - 1) * b
+  def BasisInduksi(n):
+    return n * (n+1)/ 2
