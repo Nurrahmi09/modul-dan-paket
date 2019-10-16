2923a23d10a01995bf4e647fd4ddd4def9d4ea2c
Nur Rahmi <49005228+Nurrahmi09@users.noreply.github.com>
2019-10-16 04:49:17 -0700

    Update import1.py

--- import1.py
+++ import1.py
@@ -1,2 +1,35 @@
 import nurrahmi
 def main():
+  #persamaan kuadrat
+  a = 6
+  b = 4
+  c = 2
+  
+  PK = nurrahmi.PersamaanKuadrat(a,b,c)
+  print("PERSAMAAN KUADRAT")
+  print("a\t : ", a)
+  print("b\t : ", b)
+  print("c\t : ", c)
+  print("Persamaan Kuadrat\t = ", PK)
+  
+  #Barisan Aritmatika
+  a = 9
+  b = 7
+  n = 5
+  
+  BA = nurrahmi.BarisanAritmatika (a, b, n)
+  print("BARISAN ARITMATIKA")
+  print("a\t : ", a)
+  print("b\t : ", b)
+  print("n\t : ", n)
+  print("Barisan Aritmatika\t = ", BA)
+  
+  #Basis Induksi
+  n = 9
+  
+  basis = nurrahmi.BasisInduksi(n)
+  print("BASIS INDUKSI")
+  print("n\t : ", n)
+  print("Basis Induksi\t = ", basis)
+if __name__ == "__main__":
+  main()
